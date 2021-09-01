c3500694501fee75ecb458f738793e1b4374003e
Hüseyin Kadıoğlu <huseyin.kadioglu@linktera.com.tr>
2021-09-02 01:34:20 +0300

    36'dan devam edilecek

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,13 +3,14 @@ import "./ExpenseItem.css";
 const ExpenseItem = () => {
 
     // JSX, ana bir element dondurmeli. Bu yuzden aux kullaniyoruz.
+    // #class reserved word olduğu için className kullanılıyor.
     return (
         <div className="expense-item">
-            <div className="ex"> March 28th 2021 </div>
-           <div className="expense-item__description">
-               <h2> Car Insurance </h2>
-               <div className="expense-item__price"> 294.67 </div>
-           </div>
+            <div className="ex"> March 28th 2021</div>
+            <div className="expense-item__description">
+                <h2> Car Insurance </h2>
+                <div className="expense-item__price"> 294.67</div>
+            </div>
         </div>
     );
 
