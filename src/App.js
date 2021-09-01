b23bd51e473b02f55e2ab6c7b10ca6a457d63ca3
Hüseyin Kadıoğlu <huseyin.kadioglu@linktera.com.tr>
2021-09-02 01:07:11 +0300

    cleaning shits

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-          Learn React
-    </div>
-  );
+    return (
+        <>
+            Learn React
+        </>
+    );
 }
 
 export default App;
